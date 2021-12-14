2a37895ea8d244fc50fc78d19b040e183024e307
maaryaamasl <96077582+maaryaamasl@users.noreply.github.com>
2021-12-13 21:13:29 -0600

    Add files via upload

---
+++ last-word-size-inString.py
@@ -0,0 +1,11 @@
+user_sentence =  input("Please enter a sentence:")
+
+usr=user_sentence.strip()
+
+count=0
+for i in range(len(usr)):
+    if usr[i]==" ":
+        count=0
+    else:
+        count+=1
+print("len of last word is:", l)
